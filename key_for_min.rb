e6637180c2ed7bb978e3f7bbbc4f8b44448b9425
Roy  Flores <royfloresnyc@gmail.com>
2020-07-21 17:31:33 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
   min_value_key = nil 
   name_hash.each do |key, value|
-    if min_value_key == nil || name_hash[min_value_key] < value
+    if min_value_key == nil || value < name_hash[min_value_key]
       min_value_key = key 
     end
   end 
